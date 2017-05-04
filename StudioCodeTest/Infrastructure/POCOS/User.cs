bb867245e55127d1df472ca995ca0235f35591eb
danielwidegren <danielwidegren@gmail.com>
2017-05-04 14:29:22 +0200

    Add files via upload

---
+++ StudioCodeTest/Infrastructure/POCOS/User.cs
@@ -0,0 +1,12 @@
+namespace StudioCodeTest.Infrastructure.POCOS
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string GivenName { get; set; }
+        public string SurName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+    }
+}
